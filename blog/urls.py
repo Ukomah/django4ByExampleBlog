19b5f0a6547b84a608582a0b9168193fcba6cf48
Ukomah <henryukomah@gmail.com>
2023-05-04 15:54:07 +0100

    creating comment model

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.postList, name='postList'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.postDetails, name='postDetails'),
-    path('<int:post_id>/share/', views.postShare, name='postShare'),
+    path('<int:post_id>/share/', views.sharePost, name='sharePost'),
 ]
