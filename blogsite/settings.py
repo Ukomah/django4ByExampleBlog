f5f078b8a6e6001c41085c6b2695f961003a225c
Ukomah <henryukomah@gmail.com>
2023-06-06 23:55:00 +0100

    adding logo

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -37,6 +37,7 @@ SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
+   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -155,3 +156,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
